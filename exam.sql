8731d2c28d6e576e362b30140cfe49f782242243
Igor Samoylenko <antonych@yandex.ru>
2019-09-10 20:19:31 +0300

    Перенос файлов

--- exam.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS example;
-USE example;
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) COMMENT 'Имя пользователя'
-) COMMENT = 'Пользователи';
